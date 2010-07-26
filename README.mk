e743682185713631bf4a5d5c0ee8074c524723be
Lorenzo Pisani <zeelot3k@gmail.com>
2010-07-26 00:31:08 -0700

    committing some global and php snippets, adding some info to readme (not complete)

---
+++ README.mk
@@ -0,0 +1,16 @@
+# Gedit Snippets
+
+## PHP
+
+- kdef
+~~~
+<?php defined('SYSPATH') or die('No direct script access.');
+~~~
+- kdoc
+~~~
+/**
+ * ${1:description}
+ *
+ * @return  ${2:void}
+ */
+~~~
